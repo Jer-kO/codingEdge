e5b18888f00465ea166afce927eb972157efb76e
William Zhang <william.zhang.285@gmail.com>
2015-09-08 16:48:08 -0700

    Changing while to Ex6, and for to Ex7, and adding some more questions

--- exercises/src/exercises/Ex7For.java
+++ exercises/src/exercises/Ex7For.java
@@ -1,15 +1,29 @@
 package exercises;
 
-public class Ex6For {
-	
-	public Ex6For() {}
+public class Ex7For {
+
+	/* Given a positive integer, return a new array with n elements,
+	 * containing the numbers 0, 1, 2, ... (n-1)
+	 * 
+	 * Note: same as the question in Ex6, but use for-loops this time!
+	 * 
+	 * Input: 4
+	 * Output: {0, 1, 2, 3}
+	 * */
+	public static int[] q1_createArrayOfIndices(int n) {
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = i;
+		}
+		return arr;
+	}
 	
 	/* Given an positive integer n, return a string with n number of 1's
 	 * 
 	 * Input: 4
 	 * Output: "1111"
 	 * */
-	public static String q1_numberOfOnes(int n) {
+	public static String q2_numberOfOnes(int n) {
 		String ones = "";
 		for (int i = 0; i < n; i++) {
 			ones = ones + "1";
@@ -23,7 +37,7 @@ public class Ex6For {
 	 * Input: {1, -3, 5, -7, 9, -11, 13};
 	 * Console Output: true
 	 * */
-	public static boolean q2_largerThan10(int[] strArr) {
+	public static boolean q3_largerThan10(int[] strArr) {
 		for (int i = 0; i < strArr.length; i++) {
 			if (strArr[i] > 10) {
 				return true;
@@ -38,7 +52,7 @@ public class Ex6For {
 	 * Input: {1, -4, 10, 3, 22, -30}
 	 * Output: 22
 	 * */
-	public static int q3_getLargestInt(int[] strArr) {
+	public static int q4_getLargestInt(int[] strArr) {
 		int largestSoFar = strArr[0];
 		for (int i = 1; i < strArr.length; i++) {
 			if (strArr[i] > largestSoFar) {
@@ -56,7 +70,7 @@ public class Ex6For {
 	 *         {-5, -1, 3, 43, 2}}
 	 * Output: 44
 	 * */
-	public static int q4_getLargestIntIn2DArray(int[][] str2DArr) {
+	public static int q5_getLargestIntIn2DArray(int[][] str2DArr) {
 		int largestSoFar = str2DArr[0][0];
 		for (int i = 0; i < str2DArr.length; i++) {
 			// Note: could also call forQ3 instead
@@ -75,7 +89,7 @@ public class Ex6For {
 	 * Input:{1, 2, 3, 4, 5}
 	 * Output: 15
 	 * */
-	public static int q5_sumOfElements(int[] strArr) {
+	public static int q6_sumOfElements(int[] strArr) {
 		int sum = 0;
 		for (int i = 0; i < strArr.length; i++) {
 			sum += strArr[i];
@@ -93,10 +107,10 @@ public class Ex6For {
 	 *         {-5, -1, 3, 43, 2}}
 	 * Output: 42
 	 * */
-	public static int q6_sumOfLargestRow(int[][] str2DArr) {
-		int largestSum = q5_sumOfElements(str2DArr[0]);
+	public static int q7_sumOfLargestRow(int[][] str2DArr) {
+		int largestSum = q6_sumOfElements(str2DArr[0]);
 		for (int i = 1; i < str2DArr.length; i++) {
-			int rowSum = q5_sumOfElements(str2DArr[i]);
+			int rowSum = q6_sumOfElements(str2DArr[i]);
 			if (rowSum > largestSum) {
 				largestSum = rowSum;
 			}
